6dcd95356a25fc182fbb915a578182c7d3ec9ffa
RonaldoML <rmonserr@espol.edu.ec>
2020-12-25 03:14:54 -0500

    Aplicación terminada

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>{{ $store.state.msg }}</h1>
+    <h1>{{ $store.getters.nombreCompleto }}</h1>
+
+    <input type="text" v-model="amigo">
+    <button @click="addAmigo">Agregar amigo</button>
+
+    <h2> Amigos </h2>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
+      <li v-for="(amigo, key) in $store.state.amigos" :key="key">
+        {{ amigo }}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <input type="text" v-model="amigoToRemove">
+    <button @click="remAmigo">Quitar amigo</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      amigo:'',
+      amigoToRemove:'',
+    }
+  },
+  methods: {
+    addAmigo(){
+      this.$store.dispatch('addAmigoAction', this.amigo);
+      this.amigo = '';
+    },
+    remAmigo(){
+      const a = this.$store.state.amigos.indexOf(this.amigoToRemove);
+      if(a != -1){
+        this.$store.dispatch('removeAmigoAction', a);
+      }else{
+        this.$toasted.success('hello billo',{action : {
+        text : 'x',
+        onClick : (e, toastObject) => {
+            toastObject.goAway(0);
+        }
+    },})
+      }
+      this.amigoToRemove = '';
+    }
+  },
 }
 </script>
 
@@ -47,10 +58,6 @@ h3 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    msg: 'VueX desde Ceros',
+    nombre: 'Ronaldo',
+    apellido: 'Monserrate',
+    amigos: [],
   },
   mutations: {
+    addAmigo(state, amigo) {
+      state.amigos = [amigo, ...state.amigos];
+    },
+    removeAmigo(state, index){
+      state.amigos.splice(index, 1);
+    }
   },
   actions: {
+    addAmigoAction(context, amigo) {
+      context.commit('addAmigo', amigo);
+    },
+    removeAmigoAction(context, index){
+      context.commit('removeAmigo', index);
+    }
   },
   modules: {
-  }
+  },
+  getters: {
+    mensaje(state) {
+      return state.msg;
+    },
+    nombreCompleto(state) {
+      return `${state.nombre} ${state.apellido}`;
+    }
+  },
 })
